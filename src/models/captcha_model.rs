89ee229a4f5de2dffcc2d9253ea00c30350b3eb9
Nadu_Dev <inbox.nadun@gmail.com>
2026-02-23 14:31:21 +0530

    feat: extend GoogleCaptchaVerifyResponse with additional fields

--- src/models/captcha_model.rs
+++ src/models/captcha_model.rs
@@ -5,7 +5,14 @@ pub struct CaptchaVerifyRequest {
     pub captcha: Option<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct GoogleCaptchaVerifyResponse {
     pub success: bool,
+    // for furure use, currently not used
+    pub challenge_ts: Option<String>,
+    pub hostname: Option<String>,
+    #[serde(rename = "error-codes")]
+    pub error_codes: Option<Vec<String>>,
+    pub score: Option<f64>,
 }
